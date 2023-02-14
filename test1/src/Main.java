a56fc16b5a2efa0b81b8948336eec691d83f5845
Yunbin <yunbin@YunbindeMacBook-Air.local>
2023-02-14 13:07:52 +0800

    Initial commit

---
+++ test1/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hhh");
+        System.out.println("Hello world!");
+        int i = 3;
+        System.out.println(i);
+    }
+}
